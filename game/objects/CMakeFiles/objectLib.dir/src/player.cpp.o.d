107384286fff531221e259dde31369bc081f93db
Arya Bhattacharjee <aryabhatt2004@gmail.com>
2023-11-19 18:41:41 -0800

    Remove unnecessary files + update git ignore

--- game/objects/CMakeFiles/objectLib.dir/src/player.cpp.o.d
+++
@@ -1,3 +0,0 @@
-game/objects/CMakeFiles/objectLib.dir/src/player.cpp.o: \
-  /Users/aryabhattacharjee/Documents/CS100/final-project-jmuth004-ssan003-abhat090-thass004/game/objects/src/player.cpp \
-  /Users/aryabhattacharjee/Documents/CS100/final-project-jmuth004-ssan003-abhat090-thass004/game/objects/headers/player.hpp
