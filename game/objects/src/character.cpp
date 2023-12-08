714c8e85d2aefe460f6e4cfe734f47ecdbe0c31f
apalu003 <apalu003@ucr.edu>
2023-12-07 23:42:40 -0800

    character.cpp update

--- game/objects/src/character.cpp
+++ game/objects/src/character.cpp
@@ -2,32 +2,10 @@
 #include <iostream>
 using namespace std;
 
-void Character::setName(const string& name) {this->name = name;}
-void Character::setHP(int HP) {this->HP = HP;}
-void Character::setDef(int def) {this->def = def;}
-void Character::setAtk(int atk) {this->atk = atk;}
-
-void Character::chAtkStats(int atk) {
-    this->atk += atk;
-}
-void Character::chDefStats(int def) {
-    this->def += def;
-}
-void Character::heal(int increaseHP) {
-    HP += increaseHP;
-}
-void Character::damage(int damage) {
-    HP -= damage;
-}
 void Character::setName(const string& name) {
     this->name = name;
 }
+
 void Character::setHP(int HP) {
     this->HP = HP;
-}
-void Character::setAtk(int atk) {
-    this->atk = atk;
-}
-void Character::setDef(int def) {
-    this->def = def;
 }
