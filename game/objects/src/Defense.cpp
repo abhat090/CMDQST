c39387f755ef7709a880715a15a914b79e05b324
Sujith Sanniboyina <ssann003@xe-01.cs.ucr.edu>
2023-12-08 00:59:51 -0800

    Fixed parameters for defense.cpp and defense.hpp

--- game/objects/src/Defense.cpp
+++ game/objects/src/Defense.cpp
@@ -9,15 +9,15 @@ int Defense::getName() {
 int Defense::setName() {
    name = "Defense";
 }
-int Defense::mildDefense() {
+int Defense::mildDefense(Player * user) {
    intensity /= 1.2;
    user->HP -= cost * 0.6;
 }
-int Defense::mediumDefense() {
-   intensity *= 1.5;
+int Defense::mediumDefense(Player * user) {
+   intensity /= 1.5;
    user->HP -= cost;
 }
-int Defense::heavyDefense() {
+int Defense::heavyDefense(Player * user) {
    intensity /= 2;
    user->HP -= cost * 1.5;
 }
